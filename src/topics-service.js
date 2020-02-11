80af38932ba187804614091d1e7dbc784248bc67
kraigwilliams <krgwilliams@gmail.com>
2020-02-10 20:19:40 -0400

    topics service crud setup complete

--- src/topics-service.js
+++ src/topics-service.js
@@ -1,8 +1,29 @@
 const TopicsService={
     getAllTopics(knex){
-    return knex.select('*').from('overlays_topics')}
-},
-insertTopic(knex, newTopic){
+    return knex.select('*').from('overlays_topics')
+}
+,
 
+insertTopic(knex, newTopic){
+return knex
+.insert(newTopic)
+.into('overlays_topics')
+.returning("*")
+},
+getById(knex,id){
+    return knex.from('overlays_topics').select("*").where('id', id).first()
+},
+
+deleteTopic(knex,id){
+    return knex('overlays_topics')
+    .where({id})
+    .delete()
+},
+updateTopic(knex,id, newTopicFields){
+    return knex('overlays_topics')
+    .where({id})
+    .update(newTopicFields)
+}
 }
 
+module.exports= TopicsService;
