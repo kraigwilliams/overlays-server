8d3ef12b74cefe09dba6fb17c1af749d01fecaaf
kraigwilliams <krgwilliams@gmail.com>
2020-02-12 08:43:30 -0400

    login is working on frontend and back end

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ module.exports={
 PORT: process.env.PORT || 8000,
 NODE_ENV: process.env.NODE_ENV || 'development',
 DB_URL:process.env.DB_URL || "postgresql://overlays@localhost/overlays_server",
-JWT_SECRET: process.env.JWT_SECRET 
+JWT_SECRET: process.env.JWT_SECRET || "kraighasalittlesecret" 
 }
