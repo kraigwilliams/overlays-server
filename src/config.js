3a70376349b6b289eddc64cede9167f8eb6cdd7a
KRAIG WILLIAMS <williamskrg@gmail.com>
2020-02-19 17:53:00 -0400

    deployment

--- src/config.js
+++ src/config.js
@@ -3,6 +3,6 @@
 module.exports={
 PORT: process.env.PORT || 8000,
 NODE_ENV: process.env.NODE_ENV || 'development',
-DB_URL:process.env.DB_URL || "postgresql://overlays@localhost/overlays_server",
+DB_URL:process.env.DB_URL || "https://kraig-overlays-server.herokuapp.com/",
 JWT_SECRET: process.env.JWT_SECRET || "kraighasalittlesecret" 
 }
